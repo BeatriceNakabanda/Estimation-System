369d0011da3b11ff45cbd9c04ddbaf3b1fc2f370
kimbugweronnie <31738584+kimbugweronnie@users.noreply.github.com>
2019-11-08 11:38:23 +0300

    Updating server.js
    Updating server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const cors = require("cors");
-const passport = require("passport");
+
 
 const app = express();
 //middlewares
@@ -18,6 +18,24 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+// Database connection
+const db = require("./config/uri").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log(`Database connection is ${db}`);
+  })
+  .catch(err => {
+    console.log(`error ${err}`);
+  });
+//localhost
+const users = require("./routes/api/user");
+app.use("/api/user", users);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Served ${PORT}`);
+});
 
 
 
