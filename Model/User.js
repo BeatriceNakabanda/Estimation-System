8933b8fb20d6216bc1e2c6321e8a0b952e5cdcfa
kimbugweronnie <kimbugweronnie@gmail.com>
2019-11-13 12:07:58 +0300

    modifying mongoose model

--- Model/User.js
+++ Model/User.js
@@ -2,26 +2,7 @@ const mongoose = require("mongoose");
 //creating schema object called schema
 const Schema = mongoose.Schema;
 // Create User schema which is the object of schema
-const UserSchema = new Schema({
-  title: {
-    type: String,
-     required: "this is required"
-  },
-
-  Project: {
-    type: String,
-    required: "this is required"
-  },
-  Developer: {
-    type: String,
-    required: "this is required"
-  },
-  
-  dueDate: {
-    type: Date,
-    required: "this is required"
-  }
-});
+const UserSchema = new Schema({});
 
 module.exports = User = mongoose.model("estimate", UserSchema);
 // estimate is the name of the table/collection
