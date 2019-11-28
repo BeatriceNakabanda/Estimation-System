e694df0583c03887067cd23f65b156bbcee3e1f3
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2019-11-28 17:54:14 +0300

    edits added to models

--- server/models/users_model.js
+++ server/models/users_model.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 //roles schema
 const UserRolesSchema = new Schema({
-    developer : {type: String, required: true}
+    name: String
 });
 
 //user schema
@@ -28,3 +28,49 @@ const UserSchema = new Schema({
  const User = mongoose.model('User', UserSchema);
 
  module.exports = User;
+
+//declaring options
+/* const options = {discriminatorKey: 'kind'};
+
+const userSchema = new Schema({
+    name: String,
+    password: String,
+    email: String,
+    role: 
+}, options);
+
+const User = mongoose.model('User', userSchema);
+
+const Developer = User.discriminator('Developer',
+new Schema({
+    role : String,
+}, options));
+
+const ProjectManager = User.discriminator('ProjectManager',
+new Schema({
+    role : String,
+}, options));
+
+module.exports = User, Developer, ProjectManager; */
+
+/* const usersSchema = new Schema({
+    name: String,
+    password: String,
+    email: String,
+    role: String
+});
+
+const developerSchema = new Schema({ 
+    name: String,
+    password: String,
+    role: String});
+const projectManagerSchema = new Schema({
+    name: String,
+    password: String,
+    role: String
+});
+
+
+const User = mongoose.model('User', usersSchema, 'users');
+User.Developer = mongoose.model('Developer', developerSchema, 'users');
+User.ProjectManager = mongoose.model('ProjectManager', projectManagerSchema, 'users'); */
