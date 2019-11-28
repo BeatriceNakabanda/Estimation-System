9e78599155e22e2aa878a862ef981ce1efb6d23d
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2019-11-28 16:06:24 +0300

    cleaned up routes code.

--- server/routes/api_routes.js
+++ server/routes/api_routes.js
@@ -45,40 +45,5 @@ router.post("/user", userController.createUser)
 
 //get a single projectManager
 
-//get request from the db
-/* router.get("/estimate", (req, res) => {
-  request.find({}).then(function(request) {
-    res.send(request);
-  });
-}); */
-
-//post request to the db
-/* router.post("/estimate", (req, res, next) => {
-  //sending request to the db
-  request
-    .create(req.body)
-    .then(function(estimate) {
-      res.send(estimate);
-    })
-    .catch(next);
-});  */
-
-//put request in the db
-/* router.put("/estimate/:id", (req, res) => {
-  request.findByIdAndRemove({ _id: req.params.id }, req.body).then(function() {
-    //find updated request from the db
-    request.findOne({ _id: req.params.id }).then(function(request) {
-      res.send(request);
-    });
-  });
-}); */
-
-//delete request from the db
-/* router.delete("/estimate/:id", (req, res) => {
-  request.findByIdAndRemove({ _id: req.params.id }).then(function(request) {
-    res.send(request);
-  });
-}); */
-
 //exporting router
 module.exports = router;
