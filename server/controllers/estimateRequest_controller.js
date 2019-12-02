0e896e4d61f0b84cf1ac0efdc9b48600acb3ac34
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2019-12-02 11:54:46 +0300

    Edited controllers

--- server/controllers/estimateRequest_controller.js
+++ server/controllers/estimateRequest_controller.js
@@ -5,7 +5,7 @@ mongoose.set('useFindAndModify', false);
 
 //get all estimate requests
 exports.estimatesList = function(req, res){
-  request.find({}, function (estimate, next){
+  request.find({}, function (next, estimate){
     if(next){
       res.send(next);
     }else{
@@ -17,7 +17,7 @@ exports.estimatesList = function(req, res){
 //create estimate request
 exports.createEstimate = function(req, res){
   const newEstimate = new request(req.body);
-  newEstimate.save(function(estimate, next){
+  newEstimate.save(function(next, estimate){
     if(next){
       res.send(next);
     }else{
@@ -28,7 +28,7 @@ exports.createEstimate = function(req, res){
 
 //get single estimate request
 exports.singleEstimate = function(req, res){
-  request.findById({_id: req.params.requestId}, function(estimate, next){
+  request.findById({_id: req.params.requestId}, function(next, estimate){
     if(next){
       res.send(next);
     }else{
@@ -39,7 +39,7 @@ exports.singleEstimate = function(req, res){
 
 //update single estimate request
 exports.updateEstimate = function(req, res){
-  request.findByIdAndUpdate({_id: req.params.requestId}, req.body, function(estimate, next){
+  request.findByIdAndUpdate({_id: req.params.requestId}, req.body, function(next, estimate){
     /* if(next)
         res.send(next);
         res.json(estimate) */
--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -5,7 +5,7 @@ mongoose.set('useFindAndModify', false);
 
 //get all users
 exports.usersList = function(req, res){
-  user.find({}, function (user, next){
+  user.find({}, function (next, user){
     if(next){
       res.send(next);
     }else{
@@ -16,7 +16,7 @@ exports.usersList = function(req, res){
 
 //get all users who are developers
 exports.usersDevelopers = function(req, res){
-  user.find({role: "developer"}, function(developers, next){
+  user.find({role: "developer"}, function(next, developers){
     if(next){
       res.send(next);
     }else{
@@ -28,7 +28,7 @@ exports.usersDevelopers = function(req, res){
 //create user
 exports.createUser = function(req, res){
   const newUser = new user(req.body);
-  newUser.save(function(user, next){
+  newUser.save(function(next, user){
     if(next){
       res.send(next);
     }else{
@@ -39,7 +39,7 @@ exports.createUser = function(req, res){
 
 //get single user
 exports.singleUser = function(req, res){
-  user.findById({_id: req.params.requestId}, function(user, next){
+  user.findById({_id: req.params.requestId}, function(next, user){
     if(next){
       res.send(next);
     }else{
