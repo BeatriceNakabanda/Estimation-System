b36d566ea5ef148d2e9796bbc6a5e0dfdad989fb
kimbugweronnie <kimbugweronnie@gmail.com>
2019-12-12 12:39:28 +0300

    adding developer route

---
+++ skalla_server/modules/developers_module/developers_routes.js
@@ -0,0 +1,13 @@
+//requiring dependecies
+const express = require("express");
+const router = express.Router(); //express router for routing
+const developerService = require("./developer_contoller");
+
+/* Listing Project Routes */
+
+//get all projects
+router.get("/developer", developerService.developerList);
+
+// router.post("/developer", projectService.updateDeveloper);
+
+module.exports = router;
