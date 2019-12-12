928ff05343c196ac4fc4a486ac78295a787ccc89
kimbugweronnie <kimbugweronnie@gmail.com>
2019-12-12 13:18:17 +0300

    updating package.json

--- skalla_server/modules/developers_module/developers_routes.js
+++ skalla_server/modules/developers_module/developers_routes.js
@@ -5,7 +5,7 @@ const developerService = require("./developer_contoller");
 
 /* Listing Project Routes */
 
-//get all projects
+//get all developers
 router.get("/developers", developerService.developerList);
 
 // router.post("/developer", projectService.updateDeveloper);
