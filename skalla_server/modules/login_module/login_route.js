a906dbca56aaa6eab3ab48167cf6e4db80faecfd
kimbugweronnie <kimbugweronnie@gmail.com>
2020-01-05 14:25:48 +0300

    adding to login route

--- skalla_server/modules/login_module/login_route.js
+++ skalla_server/modules/login_module/login_route.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router(); //express router for routing
-//const passport = require('passport');
-const passport = require("../../config/passport");
+
+//const passport = require("../../config/passport");
+const passport=require("passport");
 
 router.post(
-  "/login",
-  passport.authenticate("local", {
+  "/login",passport.authenticate("local", {
     successRedirect: "/api/project",
     failureRedirect: "/login",
     failureFlash: true
   })
 );
+
+module.exports = router
