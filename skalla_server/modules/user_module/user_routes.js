cc1c591660961308c3edc0929b3d8fb61c734b12
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-01-10 06:13:39 +0300

    Cleaned up user routes file.

--- skalla_server/modules/user_module/user_routes.js
+++ skalla_server/modules/user_module/user_routes.js
@@ -1,16 +1,3 @@
-/* const express = require("express");
-const router = express.Router();
-const UserController = require("./userController");
-const loginController = require("./loginController");
-router.post("/userlogin", (req, res) => loginController.loginUser(req, res));
-
-//router.get("/users", (req, res) => userController.getAllUsers(req, res));
-router.get("/users", UserController.getUsers);
-router.get("/user/:userId", UserController.singleUser);
-
-module.exports = router;
- */
-
 //requiring dependecies
 const express = require("express")
 const router = express.Router(); 
