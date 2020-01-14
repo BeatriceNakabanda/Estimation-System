f514bd987b0af2686596aa2276c9541e7d007406
kimbugweronnie <kimbugweronnie@gmail.com>
2020-01-14 12:01:01 +0300

    adding module to store for ProjectManagerNavbar.vue

--- skalla_client/src/services/AuthService.js
+++ skalla_client/src/services/AuthService.js
@@ -1,26 +1,25 @@
 //making requests to the server
-import axios from 'axios'
-import router from "../router"
+import axios from "axios";
+import router from "../router";
 
-const url = 'http://localhost:8081/api/'
+const url = "http://localhost:8081/api/";
 
-export default {    
-    login(credentials){
-        return axios
-        .post(url + 'user/userlogin', credentials)
-        .then(response => {
-            response.data
-            const role = response.data.role
-            console.log(role)
-            if(role === 'Developer'){
-                router.push('/pending-estimates')
-              }else if(role === 'Project Manager'){
-                router.push('/estimates')
-              }
-        })
-        
-    },
-    getSecretContent(){
-        return axios.get(url + 'secret-route/').then(response => response.data)
-    }
-}
+export default {
+  login(credentials) {
+    return axios.post(url + "user/userlogin", credentials).then(response => {
+      response.data;
+      const role = response.data.role;
+      const name = response.data.name;
+      console.log(role);
+      console.log(name);
+      if (role === "Developer") {
+        router.push("/pending-estimates");
+      } else if (role === "Project Manager") {
+        router.push("/estimates");
+      }
+    });
+  },
+  getSecretContent() {
+    return axios.get(url + "secret-route/").then(response => response.data);
+  }
+};
