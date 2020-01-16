27ef2b2872bdd12f52c5d88976f06cd70e643ea1
kimbugweronnie <kimbugweronnie@gmail.com>
2020-01-16 10:32:55 +0300

    deleting passport.js

--- skalla_client/src/services/AuthService.js
+++ skalla_client/src/services/AuthService.js
@@ -4,31 +4,27 @@ import router from "../router";
 
 const url = "http://localhost:8081/api/";
 
-export default {    
-    login(credentials){
-        return axios
-        .post(url + 'user/userlogin', credentials)
-        .then(response => {
-            response.data
-            const token = response.data.token
-            const role = response.data.role
-            const email = response.data.email
-            const name = response.data.name
+export default {
+  login(credentials) {
+    return axios.post(url + "user/userlogin", credentials).then(response => {
+      response.data;
+      const token = response.data.token;
+      const role = response.data.role;
+      const email = response.data.email;
+      const name = response.data.name;
 
-            const user ={
-                name,
-                email,
-                role
-            }
-            // console.log(role)
-            // console.log(response);
+      const user = {
+        name,
+        email,
+        role
+      };
+      // console.log(role)
+      // console.log(response);
 
-            return {
-                user,
-                token
-            }
-        })
-        
-    },
-
-}
+      return {
+        user,
+        token
+      };
+    });
+  }
+};
