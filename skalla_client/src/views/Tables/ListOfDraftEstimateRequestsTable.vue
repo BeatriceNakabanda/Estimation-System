fedc3a1ba42ae488f10ed80fda513d9d39077e78
EmmellaBeatrice <danahbeat@gmail.com>
2020-01-22 20:03:50 +0300

    Modified edit estimate form

--- skalla_client/src/views/Tables/ListOfDraftEstimateRequestsTable.vue
+++ skalla_client/src/views/Tables/ListOfDraftEstimateRequestsTable.vue
@@ -7,38 +7,36 @@
     </div>
 
     <div class="table-responsive table-hover">
-      <base-table class="table table-flush"
-                  :class="type === 'dark' ? 'table-dark': ''"
-                  :thead-classes="type === 'dark' ? 'thead-dark': 'thead-light'" 
-                  tbody-classes="list"
-                  :data="estimates" id="left">
-        <template  slot="columns"  >
-          <th class="bgcolor">Title</th>
-          <th class="bgcolor">Project</th>
-          <th class="bgcolor">Developer</th>
-          <th class="bgcolor">Date Created</th>
-          <th class="bgcolor"></th>
-          <!-- <th class="bgcolor">Action</th> -->
-        </template>
-          <template class="table-row" slot-scope="{row} ">
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th class="bgcolor">Title</th>
+            <th class="bgcolor">Project</th>
+            <th class="bgcolor">Developer</th>
+            <th class="bgcolor">Date Created</th>
+            <th class="bgcolor"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="estimate in estimates" :key="estimate.id">
           <td class="title">
-            {{row.title}}
+            {{estimate.title}}
           </td>
 
           <td class="project">
-            {{row.project.name}}
+            {{estimate.project.name}}
           </td>
           <td class="developer">
-            {{row.developer.name}}
+            {{estimate.developer.name}}
           </td>
           <td class="date-created">
-            {{ formatDate(row.dateCreated) }}
+            {{ formatDate(estimate.dateCreated) }}
           </td>
          
           <td >
             <span class="action-icons">
               <router-link  to="" id="view">
-                <i class="rounded-circle fas fa-pen" aria-hidden="true" id="my-icons" @click="openEditModel(row._id)"></i>
+                <i class="rounded-circle fas fa-pen" aria-hidden="true" id="my-icons" @click="openEditModel(estimate._id)"></i>
               </router-link>
               <modal :show.sync="editDraftModal" >
                       <template slot="header">
@@ -46,7 +44,7 @@
                       </template>
                       <!-- Edit Draft Estimate Request Form -->
                       <!-- <EditDraftEstimateRequestForm  /> -->
-                          <form method="POST" role="form" @submit.prevent="handleEditt()">
+                          <form method="POST" role="form" >
                             <div>
                                 <div class="row">
                                 <div class="col-sm-3">          
@@ -61,7 +59,7 @@
                                             
                                             @keypress="clearForm"
                                           >
-                                            <select class="custom-select" id="inputGroupSelect01" v-model="row.project.name">
+                                            <select class="custom-select" id="inputGroupSelect01" v-model="estimate.project.name">
                                             <option value="" disabled>Please select a project</option>
                                             <option v-for="project in projects" :key="project.id">{{project.name}}</option>
                                             </select>
@@ -80,7 +78,7 @@
                                             placeholder="Add developer here..."
                                           :class="{ 'has-error': submitting }" 
                                             >
-                                            <select class="custom-select" id="inputGroupSelect01" v-model="row.developer.name">
+                                            <select class="custom-select" id="inputGroupSelect01" v-model="estimate.developer.name">
                                                 <option value="" disabled>Please select a developer</option>
                                                 <option v-for="developer in developers" :key="developer.id">{{developer.name}}</option>
                                             </select>
@@ -105,7 +103,7 @@
                                                     placeholder="17-07-2019"
                                                     class="form-control datepicker"
                                                     :class="{ 'has-error': submitting  }"
-                                                    v-model="row.dueDate">
+                                                    v-model="estimate.dueDate">
                                         </flat-picker>
                                     </base-input>
                                 </div>
@@ -118,8 +116,8 @@
                                         <base-input alternative
                                                 class="mb-3"
                                                 placeholder="Add title here..."
-                                                v-model="row.title" 
-                                              
+                                                v-model="estimate.title" 
+                                                
                                                 :class="{ 'has-error': submitting  }"
                                             >
                                     </base-input>
@@ -132,7 +130,7 @@
                                 <div class="col-sm-12">
                                     <base-input alternative=""
                                     :class="{ 'has-error': submitting }">
-                                        <textarea rows="4" v-model="row.taskDescription" class="form-control form-control-alternative" placeholder="Add main task description here ..."></textarea>
+                                        <textarea rows="4" v-model="estimate.taskDescription"  class="form-control form-control-alternative" placeholder="Add main task description here ..."></textarea>
                                     </base-input>
                                 </div>
                                 </div>
@@ -149,33 +147,35 @@
                                 </div>
                                 <!-- <base-button class="shadow-none mt-4" type="primary" @click="addEstimate">Send request</base-button> -->
                                 <div class="col text-right">
-                                  <base-button class="shadow-none mt-4" type="primary" id="save-draft" @click="handleEditt(row._id)">Edit request</base-button>
-                                  <base-button class="shadow-none mt-4" type="primary" id="submit" @click="openSendRequestModel(row._id)">Send request</base-button>
+                                  <base-button class="shadow-none mt-4" type="primary" id="save-draft" @click="handleEditt(estimate._id)">Edit request</base-button>
+                                  <base-button class="shadow-none mt-4" type="primary" id="submit" @click="openSendRequestModel(estimate._id)">Send request</base-button>
                                 </div>
                                 </div>
+                                <!-- <p>{{newEstimateId}}</p> -->
                             </form>
                   </modal>
             </span>
              <span class="action-icons" id="view" >
-              <i class="rounded-circle ni ni-curved-next" aria-hidden="true" id="my-icons" @click="handleSendRequest2(row._id)"></i>
+              <i class="rounded-circle ni ni-curved-next" aria-hidden="true" id="my-icons" @click="handleSendRequest2(estimate._id)"></i>
               <modal :show.sync="sendEstimateRequest" >
                 <template slot="header">
                     <h3 class="modal-title " id="exampleModalLabel">Send Estimate Request</h3>
                 </template>
-                <P><b id="details">Project:</b>  {{row.project.name}}</P>
-                <p><b id="details">Developer:</b>  {{row.developer.name}}</p>
-                <p><b id="details">Due Date:</b>  {{row.dueDate}}</p>
-                <p><b id="details">Title: </b>  {{row.title}}</p>
-                <p><b id="details">Main Task Description:</b>  {{row.taskDescription}}</p>
-                <base-button class="shadow-none mt-4" type="primary" @click="handleSendRequest2(row._id)">Send request</base-button>
+                <P><b id="details">Project:</b>  {{estimate.project.name}}</P>
+                <p><b id="details">Developer:</b>  {{estimate.developer.name}}</p>
+                <p><b id="details">Due Date:</b>  {{estimate.dueDate}}</p>
+                <p><b id="details">Title: </b>  {{estimate.title}}</p>
+                <p><b id="details">Main Task Description:</b>  {{estimate.taskDescription}}</p>
+                <base-button class="shadow-none mt-4" type="primary" @click="handleSendRequest2(estimate._id)">Send request</base-button>
                 
               </modal>
             </span>
             
           </td>
-          </template>
+          </tr>
+        </tbody>
 
-      </base-table>
+      </table>
     </div>
 
     <!-- <div class="card-footer d-flex justify-content-end"
@@ -186,13 +186,11 @@
   </div>
 </template>
 <script>
-
 // import EditDraftEstimateRequestForm from "../Forms/EditDraftEstimateRequestForm";
 import flatPicker from "vue-flatpickr-component";
 import "flatpickr/dist/flatpickr.css";
 import { format } from 'date-fns';
 import axios from 'axios'
-
   export default {
     name: 'drafts-estimates-table',
     components: {
@@ -217,6 +215,9 @@ import axios from 'axios'
         success: false,
         projects: [],
         developers: [],
+        taskDescription: '',
+
+        // newEstimateId: '',
            
         estimate:
           {
@@ -234,42 +235,39 @@ import axios from 'axios'
           openEditModel(estimateId){
             this.editDraftModal = true
             // this.estimateId
-            console.log(estimateId)
+            // console.log(estimateId)
             
             return estimateId
           },
           handleEditt(estimateId){
             this.submitting = true
             this.clearForm()
-            // validating empty inputs
-              // if(this.invalidProjectName || this.invalidDueDate || this.invalidTitle || this.invalidTaskDescription)
-              //     {
-              //         this.error = true
-              //         return
-              //     }
-            let newEstimateId = this.openEditModel(estimateId)
             
+           
+            let newEstimateId = this.openEditModel(estimateId)
             console.log(newEstimateId)
             // debugger
-            let editedEstimate = {
-                  project: this.estimate.project.id,
-                  developer: this.estimate.developer.id,
-                  dueDate: this.estimate.dueDate,
-                  title: this.estimate.title,
-                  taskDescription: this.estimate.taskDescription,
-                  projectManager: this.$store.getters.getUser.id
+            const title = this.estimate.title
 
-              }
-            axios.put(`http://localhost:8081/api/estimate-request/` + newEstimateId , editedEstimate)
-                  .then((response) =>{
-                      console.log(response);
-                  })
-                  .catch((error) => {
-                      console.log(error);
-                  });
-
-                  this.success = true
-                  this.error = false
+            // const editedEstimate = {
+            //       project: this.estimate.project.id,
+            //       developer: this.estimate.developer.id,
+            //       dueDate: this.estimate.dueDate,
+            //       title = this.estimate.title,
+            //       taskDescription = this.estimate.taskDescription,
+            //       projectManager = this.$store.getters.getUser.id
+            //   }
+              console.log(this.taskDescription)
+              // console.log(editedEstimate)
+          //   axios.put(`http://localhost:8081/api/estimate-request/` + newEstimateId , editedEstimate)
+          //         .then((response) =>{
+          //             console.log(response);
+          //         })
+          //         .catch((error) => {
+          //             console.log(error);
+          //         });
+          //         this.success = true
+          //         this.error = false
      
           },
           handleCancel(){
@@ -279,23 +277,22 @@ import axios from 'axios'
           async handleSendRequest(estimateId){
             // let newEstimateId = this.openEditModel(estimateId)
             let editedEstimate = {
-                  project: this.selectedProject.id,
-                  developer: this.selectedDeveloper.id,
+                  project: this.estimate.project.id,
+                  developer: this.estimate.developer.id,
                   dueDate: this.estimate.dueDate,
                   title: this.estimate.title,
                   taskDescription: this.estimate.taskDescription,
                   projectManager: this.$store.getters.getUser.id,
                   status: this.estimate.status = "Submitted"
-
               }
-            axios.put(`http://localhost:8081/api/estimate-request/` + estimateId , editedEstimate)
-                  .then((response) =>{
-                      console.log(response);
-                  })
-                  .catch((error) => {
-                      console.log(error);
-                  });
-
+              console.log(editedEstimate)
+            // axios.put(`http://localhost:8081/api/estimate-request/` + estimateId , editedEstimate)
+            //       .then((response) =>{
+            //           console.log(response);
+            //       })
+            //       .catch((error) => {
+            //           console.log(error);
+            //       });
           },
           openSendRequestModel(estimateId){
             this.sendEstimateRequest = true
@@ -306,7 +303,6 @@ import axios from 'axios'
             
             let editedEstimate = {
                   status: this.estimate.status = "Submitted"
-
               }
             axios.put(`http://localhost:8081/api/estimate-request/` + newEstimateId , editedEstimate)
                   .then((response) =>{
@@ -315,9 +311,7 @@ import axios from 'axios'
                   .catch((error) => {
                       console.log(error);
                   });
-
           },
-
           clearForm(){
                 this.success = false
                 this.error = false
@@ -330,7 +324,7 @@ import axios from 'axios'
       try{
         const response = await axios.get(`http://localhost:8081/api/projects`)
         const resp = await axios.get(`http://localhost:8081/api/users/developers`)
-
+        
         this.projects = response.data;
         this.developers = resp.data;
         // window.location.reload();
@@ -355,7 +349,6 @@ import axios from 'axios'
 .table-row{
   cursor:pointer;
 }
-
 .spacing{
   padding-left: 16px;
   padding-right: 16px;
@@ -370,7 +363,6 @@ import axios from 'axios'
   color: #5e72e4;
   border: 1px solid #5e72e4;
 }
-
 /* First column of table font adjustment */
 .text-sm {
   font-weight: 400;
@@ -382,12 +374,10 @@ import axios from 'axios'
   padding: 6px;
   
 } */
-
 /* Status column font size adjustment */
 span .status{
   font-size: 13px; 
 }
-
 .bgcolor {  
   background: #e7eaec !important;
 }
@@ -422,7 +412,6 @@ base-button{
   border-color: #d10572;
   color: #eee7eb;
 }
-
 /* Desktops and laptops ----------- */
 @media only screen  and (min-width : 1224px) {
 .card{
