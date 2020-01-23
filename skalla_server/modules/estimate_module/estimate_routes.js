8951b6b1baf31968881c7d1f92fbbd46f11216a0
kimbugweronnie <kimbugweronnie@gmail.com>
2020-01-23 19:00:06 +0300

    adding endpoints for estimate

--- skalla_server/modules/estimate_module/estimate_routes.js
+++ skalla_server/modules/estimate_module/estimate_routes.js
@@ -14,59 +14,69 @@ router.get(
 //get all estimated  estimates information
 router.get(
   "/estimated-estimates/:requestedId",
-  //loginController.CheckToken,
+  loginController.CheckToken,
   estimateController.estimatedList
-);
-//getting draft estimates
-router.get(
-  "/draft-estimates/:requestedId",
-  loginController.CheckToken,
-  estimateController.estimatedraftList
-);
-
-//get all pending estimates
-router.get(
-  "/pending-estimate/:requestedId",
-  loginController.CheckToken,
-  estimateController.estimatePendingList
-);
-//get all submitted estimates
-router.get(
-  "/submitted-estimates/:requestedId",
-  loginController.CheckToken,
-  estimateController.estimatesubmittedList
-);
-//changing to draft
-router.put(
-  "/change-draft/:requestedId",
-  loginController.CheckToken,
-  estimateController.changingStatusToDraft
-);
-//changing to submitted
-router.put(
-  "/change-submitted/:requestedId",
-  loginController.CheckToken,
-  estimateController.changingStatusToSubmitted
-);
-//get a single estimate
-router.get(
-  "/estimate/:requestId",
-  loginController.CheckToken,
-  estimateController.singleEstimate
 );
 
 //create a single estimate
 router.post(
-  "/estimate",
+  "/create-estimate/:requestedId",
   loginController.CheckToken,
   estimateController.createEstimate
 );
-
-//update a single estimate
-router.put(
-  "/estimate/:requestId",
+//getting all to estimates
+router.get(
+  "/get-all-estimates/:requestedId",
   loginController.CheckToken,
-  estimateController.updateEstimate
+  estimateController.estimatesList
 );
+
+//editing an estimate
+router.put(
+  "/editing-estimate/:requestedId",
+  //loginController.CheckToken,
+  estimateController.editingEstimate
+);
+
+//get a single estimate
+router.get(
+  "/estimate/:requestId",
+  //loginController.CheckToken,
+  estimateController.singleEstimate
+);
+
+//getting draft estimates
+// router.get(
+//   "/draft-estimates/:requestedId",
+//   //loginController.CheckToken,
+//   estimateController.estimatedraftList
+// );
+
+//get all pending estimates
+// router.get(
+//   "/pending-estimate/:requestedId",
+//   loginController.CheckToken,
+//   estimateController.estimatePendingList
+// );
+// //get all submitted estimates
+// router.get(
+//   "/submitted-estimates/:requestedId",
+//   loginController.CheckToken,
+//   estimateController.estimatesubmittedList
+// );
+
+//changing to submitted
+// router.put(
+//   "/change-submitted/:requestedId",
+//   loginController.CheckToken,
+//   estimateController.changingStatusToSubmitted
+// );
+
+// //update a single estimate
+// router.put(
+//   "/estimate/:requestId",
+//   loginController.CheckToken,
+//   estimateController.updateEstimate
+// );
 
 module.exports = router;
