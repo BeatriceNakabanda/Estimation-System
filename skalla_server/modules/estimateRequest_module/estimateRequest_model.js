aa54ea834130c459feb6e228c4721dbcb92669cc
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-01-24 15:58:55 +0300

    Edits to ER model and ER table

--- skalla_server/modules/estimateRequest_module/estimateRequest_model.js
+++ skalla_server/modules/estimateRequest_module/estimateRequest_model.js
@@ -4,18 +4,17 @@ const Schema = mongoose.Schema;
 
 //Defining EstimateRequestSchema
 const EstimateRequestSchema = new Schema({
-  project: { type: Schema.ObjectId, ref: "Project" },
-  developer: { type: Schema.ObjectId, ref: "User" },
+  project: { type: Schema.ObjectId, ref: "Project", required: true },
+  developer: { type: Schema.ObjectId, ref: "User", required: true },
   title: { type: String },
-  projectManager: { type: Schema.ObjectId, ref: "User" },
+  projectManager: { type: Schema.ObjectId, ref: "User", required: true },
   dueDate: { type: String },
   taskDescription: { type: String },
   status: {
     type: String,
-    enum: ["Draft", "Submitted", "Estimated", "In Progress"]
+    enum: ["Draft", "Submitted", "Estimated"]
   },
-  dateCreated: { type: Date, default: Date.now },
-  estimateId: { type: Schema.ObjectId, ref: "Estimate" }
+  dateCreated: { type: Date, default: Date.now }
 });
 
 //exporting estimateRequest model
