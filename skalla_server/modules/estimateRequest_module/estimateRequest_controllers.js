bc0c666d2cd60820e9afef9129517e6eabd8974e
kimbugweronnie <kimbugweronnie@gmail.com>
2020-01-30 14:27:41 +0300

    adding promises to the end point for create estimate

--- skalla_server/modules/estimateRequest_module/estimateRequest_controllers.js
+++ skalla_server/modules/estimateRequest_module/estimateRequest_controllers.js
@@ -95,7 +95,7 @@ exports.estimateRequestList = function(req, res, next) {
 
 exports.createEstimateRequest = async function(req, res) {
   const estimateRequest = new EstimateRequest(req.body);
-  console.log("ask");
+
   try {
     const createdEstimate = await estimateRequest.save(estimateRequest);
 
