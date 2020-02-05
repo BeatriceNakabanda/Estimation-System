bad975f229eb090468d719e55a5c58a17f34d912
EmmellaBeatrice <danahbeat@gmail.com>
2020-02-05 16:54:52 +0300

    config file

--- skalla_client/vue.config.js
+++ skalla_client/vue.config.js
@@ -1,7 +1,9 @@
 const webpack = require('webpack');
 const isProd = process.env.NODE_ENV === "production";
+const path = require('path')
 
 module.exports = {
+  outputDir: path.resolve(__dirname, '../skalla_server/public'),
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
